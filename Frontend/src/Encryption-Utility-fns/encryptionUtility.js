002a9c357c866927d4b0a67d86b3f8fc0a68dc02
Nikhil Sirsat <sirsatnikhil617@gmail.com>
2025-03-21 09:13:10 +0530

    type const

--- Frontend/src/Encryption-Utility-fns/encryptionUtility.js
+++ Frontend/src/Encryption-Utility-fns/encryptionUtility.js
@@ -1,5 +1,5 @@
 import CryptoJS from 'crypto-js';
-secretKey = import.meta.env.VITE_AES_KEY;
+const secretKey = import.meta.env.VITE_AES_KEY;
 
 // Encrypt a message
 export const encryptMessage = (message) => {
